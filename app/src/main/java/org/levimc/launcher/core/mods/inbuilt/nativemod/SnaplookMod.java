f19f3609c8f1ea6c861f6861dcf0ff322da731a6
galko121228-design <galko121228@gmail.com>
2026-07-02 02:02:25 +0500

    Добавил заглушки для недостающих методов ZoomMod и SnaplookMod

--- app/src/main/java/org/levimc/launcher/core/mods/inbuilt/nativemod/SnaplookMod.java
+++ app/src/main/java/org/levimc/launcher/core/mods/inbuilt/nativemod/SnaplookMod.java
@@ -7,6 +7,16 @@ public class SnaplookMod {
         return false;
     }
 
+    // Заглушки для методов, вызываемых из оверлея
+    public static void nativeOnKeyDown() {
+        // ничего не делаем
+    }
+
+    public static void nativeOnKeyUp() {
+        // ничего не делаем
+    }
+
+    // Оригинальные нативные методы
     public static native boolean nativeInit();
     public static native void nativeSetSnaplookEnabled(boolean enabled);
     public static native boolean nativeIsSnaplookEnabled();
--- app/src/main/java/org/levimc/launcher/core/mods/inbuilt/nativemod/ZoomMod.java
+++ app/src/main/java/org/levimc/launcher/core/mods/inbuilt/nativemod/ZoomMod.java
@@ -7,6 +7,24 @@ public class ZoomMod {
         return false;
     }
 
+    // Заглушки для методов, которые вызываются из оверлея
+    public static void nativeSetZoomLevel(float level) {
+        // ничего не делаем
+    }
+
+    public static void nativeOnKeyDown() {
+        // ничего не делаем
+    }
+
+    public static void nativeOnKeyUp() {
+        // ничего не делаем
+    }
+
+    public static void nativeOnScroll(float delta) {
+        // ничего не делаем
+    }
+
+    // Оригинальные нативные методы (можно оставить, но они не будут использоваться)
     public static native boolean nativeInit();
     public static native void nativeSetZoomEnabled(boolean enabled);
     public static native boolean nativeIsZoomEnabled();
